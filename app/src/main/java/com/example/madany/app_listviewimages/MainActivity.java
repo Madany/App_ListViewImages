d6f50080426d4e263c30fb0e3d25e3e40c850036
MADANY <superintel96@gmail.com>
2015-09-24 05:56:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/madany/app_listviewimages/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.madany.app_listviewimages;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    ListViewAdapter adapter;
+
+    String[] titulo = new String[]{
+            "titulo1",
+            "titulo2",
+            "titulo3",
+            "titulo4",
+    };
+
+    int[] imagenes = {
+            R.drawable.imagen02,
+            R.drawable.imagen01,
+            R.drawable.imagen01,
+            R.drawable.imagen02
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView lista = (ListView) findViewById(R.id.listView1);
+        adapter = new ListViewAdapter(this, titulo, imagenes);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono"+i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono LARGO " + i, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+}
